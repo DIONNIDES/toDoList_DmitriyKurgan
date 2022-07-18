90d47a936ece6a7ba14cd92cfe28f8ce716d5e9e
DIONNIDES <dimakurgan123789@gmail.com>
2022-07-19 01:11:08 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+import TodoList, {TaskType} from "./TodoList";
+
+
+function App() {
+    const tasks: Array<TaskType> = [
+        {id: 1, title: 'HTML', isDone: true},
+        {id: 2, title: 'CSS', isDone: true},
+        {id: 3, title: 'JS/TS', isDone: false}
+    ];
+    return (
+        <div className="App">
+            <TodoList title='What to learn' tasks={tasks}/>
+            {/* <TodoList title = 'What to buy'/>
+            <TodoList title = 'What to read'/>*/}
+        </div>
+    );
+}
+
+export default App;
